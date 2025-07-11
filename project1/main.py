91d9f299fc934b1cadb8051faa9f58c64c404096
sofijasljusar <sofija.sljusar@gmail.com>
2025-07-11 11:01:57 +0300

    Complete Project 1

---
+++ project1/main.py
@@ -0,0 +1,25 @@
+import string
+
+morse_dict = {'A': '.-', 'B': '-...',
+              'C': '-.-.', 'D': '-..', 'E': '.',
+              'F': '..-.', 'G': '--.', 'H': '....',
+              'I': '..', 'J': '.---', 'K': '-.-',
+              'L': '.-..', 'M': '--', 'N': '-.',
+              'O': '---', 'P': '.--.', 'Q': '--.-',
+              'R': '.-.', 'S': '...', 'T': '-',
+              'U': '..-', 'V': '...-', 'W': '.--',
+              'X': '-..-', 'Y': '-.--', 'Z': '--..',
+              '1': '.----', '2': '..---', '3': '...--',
+              '4': '....-', '5': '.....', '6': '-....',
+              '7': '--...', '8': '---..', '9': '----.',
+              '0': '-----'}
+print("Text to Morse Code Converter")
+text = input("Write your text here: ").upper()
+
+morse_letters = []
+for symbol in text:
+    if symbol != " " and symbol not in string.punctuation:
+        morse_letters.append(morse_dict[symbol])
+
+morse_code = " ".join(morse_letters)
+print("Morse code for your text:\n", morse_code)
